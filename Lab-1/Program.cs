af32a0457165b21b94bf422ab5a980ba51d3cad1
Gravmruakyla <156399776+Gravmruakyla@users.noreply.github.com>
2024-01-12 14:15:15 +0800

    уауц

---
+++ Lab-1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество строк (n): ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите количество столбцов (m): ");
+            int m = int.Parse(Console.ReadLine());
+
+            double[,] matrix = new double[n, m];
+
+            // Ввод матрицы
+            Console.WriteLine("Введите элементы матрицы построчно:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write($"Элемент [{i + 1},{j + 1}]: ");
+                    matrix[i, j] = double.Parse(Console.ReadLine());
+                }
+            }
+
+            // Вывод матрицы по столбцам
+            Console.WriteLine("Матрица по столбцам:");
+            for (int j = 0; j < m; j++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
